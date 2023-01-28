faaf6980e27c835dd7daa1587a52f527202ecd8f
Liliya <liliya0061@gmail.com>
2023-01-27 20:38:20 -0500

    created LiliyaT

---
+++ src/test/java/com/gitclassdemo/LiliyaT.java
@@ -0,0 +1,17 @@
+package com.gitclassdemo;
+
+public class LiliyaT {
+
+    public static void main(String[] args) {
+
+        LiliyaT obj = new LiliyaT();
+
+        obj.print("Liliya");
+    }
+
+    public void print(String name){
+
+        System.out.println(name);
+    }
+    
+}
